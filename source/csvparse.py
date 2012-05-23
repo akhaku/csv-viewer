09e6e006006dee7dcf1b9fe34d5d29d299a2842c
Ammar Khaku <ammar.khaku@gmail.com>
2012-05-23 01:02:24 -0400

    Make it a bit better

--- source/csvparse.py
+++ source/csvparse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
+from sys import stdin
 
-def interpolate(row):
+def interpolate(row, num_cols, max_lengths):
     col = 0
     ret = []
     while col < num_cols:
@@ -13,26 +14,33 @@ def interpolate(row):
         col += 1
     return ret
 
-from sys import stdin
-line = stdin.readline()
-table = []
-num_cols = len(line.rstrip('\n').split(','))
-max_lengths = [0] * num_cols
+def printTable(val):
+    (max_lengths, num_cols, table) = val
+    row_width = sum(max_lengths) + num_cols + 1
+    for row in table:
+        row = interpolate(row, num_cols, max_lengths)
+        print "|%s|" % "|".join(row)
+        print "-"*row_width
 
-while line != "":
-    row = line.rstrip('\n').split(',')
-    index = 0
-    for v in row:
-        if len(v) > max_lengths[index]:
-            max_lengths[index] = len(v)
-        index += 1
-    table.append(line.rstrip('\n').split(','))
+def readFile():
     line = stdin.readline()
+    table = []
+    num_cols = len(line.rstrip('\n').split(','))
+    max_lengths = [0] * num_cols
 
-row_width = sum(max_lengths) + num_cols + 1
+    while line != "":
+        row = line.rstrip('\n').split(',')
+        index = 0
+        for v in row:
+            if len(v) > max_lengths[index]:
+                max_lengths[index] = len(v)
+            index += 1
+        table.append(line.rstrip('\n').split(','))
+        line = stdin.readline()
+    return (max_lengths, num_cols, table)
 
-for row in table:
-    row = interpolate(row)
-    print "|%s|" % "|".join(row)
-    print "-"*row_width
+def main():
+    table = readFile()
+    printTable(table)
 
+main()
